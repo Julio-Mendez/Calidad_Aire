70ac1ee121a1be4458c982fddcfb243eb54c1521
Julio Mendez <juliommen001@gmail.com>
2023-05-22 18:44:08 -0500

    nuevo archivo

--- archivo2.py
+++ archivo2.py
@@ -1,6 +1,5 @@
-vector=[1,2,3,4,5,6,6,7,8]
-cont=0
-while cont<=len(vector:
-	print(vector[cont])
-	cont+=1
-
+vector = [1, 2, 3, 4, 5, 6, 6, 7, 8]
+cont = 0
+while cont <= len(vector):
+    print(vector[cont])
+    cont += 1
